04054cca3358d43604e56324c3ec80319f6a53a0
unknown <sukhpreet-s>
2020-02-08 00:19:44 -0500

    completed resume page

--- src/components/Container/Resume/Resume.js
+++ src/components/Container/Resume/Resume.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Resume() {
-  return (
-    <>
-      <h3>Resume here!</h3>
-    </>
-  )
+import myPdf from "./resume.pdf";
+
+export default class Resume extends Component {
+  render() {
+    return (
+      <>
+        <h3>Resume here!</h3>
+        <iframe src={myPdf} title="resume" width="100%" height="90%"></iframe>
+      </>
+    );
+  }
 }
