314c1b8f2f53a3e0377c9c23cfe6bfcfb534750e
Sukhpreet-s <41968447+Sukhpreet-s@users.noreply.github.com>
2023-10-05 01:20:29 -0400

    Update text in Home.js

--- src/components/Container/Home/Home.js
+++ src/components/Container/Home/Home.js
@@ -8,10 +8,9 @@ export default function Home() {
     <div className="Home" style={{ background: "" }}>
       <h2 style={{ background: "" }}>About</h2>
       <p style={{ background: "" }}>
-        The name's Sukhpreet Singh and I'm a software / web developer. <br/><br/>
+        The name's Sukhpreet Singh and I'm a software engineer / developer. <br/><br/>
         I help people bring their ideas into the real world by using variety of technologies.<br/><br/>
-        I am currently learning about mobile development to make applications more accessible.<br/><br/>
-        If that's what you are looking for, you can contact me here.
+        I am currently learning about cloud architectures and devops to make applications more accessible.
       </p>
     </div>
   );
